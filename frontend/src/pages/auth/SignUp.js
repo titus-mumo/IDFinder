af598e9ab37732fd3cc45a01e8edfda3ef9450d9
titus <tituskennedy74@gmail.com>
2024-08-29 13:37:55 +0300

    data validation in signup #6

--- frontend/src/pages/auth/SignUp.js
+++ frontend/src/pages/auth/SignUp.js
@@ -7,14 +7,43 @@ import { useNavigate } from 'react-router-dom';
 
 export const SignUp = () => {
     const [email, setEmail] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
     const showSnackBar = useSnackbar()
     const navigate = useNavigate()
 
     const handleRegisterWithEmail = (e) => {
         e.preventDefault()
+        if (email.length === 0 || phoneNumber.length === 0 || password.length === 0 || confirmPassword.length === 0){
+            return showSnackBar("Check to fill all the fields!")
+        }
+        if(!email.includes('@')){
+            return showSnackBar("Invalid email")
+        }
+        if(phoneNumber.length !== 13){
+            return showSnackBar("Phone number is invalid")
+        }
+        if(!str(phoneNumber).startsWith('+254')){
+            return showSnackBar("Phone number should start with +254")
+        }
+        if(password.length < 8){
+            return showSnackBar("Short too password!")
+        }
+
+        if(!passwordRegex.test(password)){
+            return showSnackBar("Weak password!")
+        }
+        if(password !== confirmPassword){
+            return showSnackBar("Passwords do not match!")
+        }
+
+        const data = {
+            
+        }
         showSnackBar("Sign Up success!")
         setTimeout(() => {
             navigate('/auth/login')
@@ -31,7 +60,7 @@ export const SignUp = () => {
         <div className='hidden lg:flex justify-center items-center' >
 
         </div>
-        <div className='w-full md:w-80 p-5 px-10 md:p-0'>
+        <div className='w-4/5 sm:w-3/5 md:w-80 p-5 px-10 md:p-0'>
 
             <div>
                 <p className='text-2xl font-bold my-1'>IDFinder</p>
@@ -40,9 +69,10 @@ export const SignUp = () => {
                 <p className='my-2 text-xl font-medium'>Sign Up</p>
             </div>
             <form className='flex flex-col' onSubmit={(e) => handleRegisterWithEmail(e)}>
-                <Input className='mt-2 border-2 border-gray-200 p-2 rounded-full' type='text' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} disableUnderline></Input>
-                <Input type='password' placeholder='Enter your password' className='mt-3 border-2 border-gray-200 p-2 rounded-full' value={password} onChange={(e) => setPassword(e.target.value)} disableUnderline></Input>
-                <Input type='password' placeholder='Confirm password' className='mt-3 border-2 border-gray-200 p-2 rounded-full' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} disableUnderline></Input>
+                <Input className='mt-2 border-2 border-gray-200 px-2 py-1 rounded-full text-sm' type='text' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} disableUnderline></Input>
+                <Input className='mt-2 border-2 border-gray-200 px-2 py-1 rounded-full text-sm' type='number' placeholder='Enter your phone number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} disableUnderline></Input>
+                <Input type='password' placeholder='Enter your password' className='mt-3 border-2 border-gray-200 px-2 py-1 rounded-full text-sm' value={password} onChange={(e) => setPassword(e.target.value)} disableUnderline></Input>
+                <Input type='password' placeholder='Confirm password' className='mt-3 border-2 border-gray-200 px-2 py-1 rounded-full text-sm' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} disableUnderline></Input>
                 <Button variant='contained' type='submit' className='w-full' sx={{borderRadius: 7, paddingY: 1.5, marginTop: 4}}>Sign Up</Button>
             </form>
             <div>
